f33cd26d73cc043526ac1c4b2b89585d8d59e771
litehed <57595571+litehed@users.noreply.github.com>
2021-11-11 16:40:19 -0500

    Sets motor speed properly kind of

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/LiftSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/LiftSubsystem.java
@@ -10,18 +10,19 @@ public class LiftSubsystem extends ProfiledPIDSubsystem {
     private static double kS = 0, kG = 0, kV = 0, kA = 0;
     private final ElevatorFeedforward elevatorFeedForward = new ElevatorFeedforward(kS, kG, kV, kA);
     private static double kP = 1.0, kI = 0, kD = 0;
+    private final double distancePerPulse = 2;
 
     public LiftSubsystem(Motor liftMotor) {
         super(new ProfiledPIDController(kP, kI, kD, new TrapezoidProfile.Constraints(3.0, 3.0)), 0.0);
         this.liftMotor = liftMotor;
-        liftMotor.encoder.setDistancePerPulse(2);
+        liftMotor.encoder.setDistancePerPulse(distancePerPulse);
         setGoal(0.0);
     }
 
     @Override
     public void useOutput(double output, TrapezoidProfile.State setPoint) {
         double feedForward = elevatorFeedForward.calculate(setPoint.velocity);
-        liftMotor.set(output + feedForward);
+        liftMotor.set(((output + feedForward) / distancePerPulse) / liftMotor.ACHIEVABLE_MAX_TICKS_PER_SECOND);
     }
 
     @Override
