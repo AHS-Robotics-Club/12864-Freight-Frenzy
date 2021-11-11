e7c109b7708f45eb4c2c260924b17087d3058eff
litehed <57595571+litehed@users.noreply.github.com>
2021-11-11 16:37:07 -0500

    Reformatted Lift Files

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/LiftCommand.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/commands/LiftCommand.java
@@ -7,20 +7,22 @@ import org.firstinspires.ftc.teamcode.subsystems.LiftSubsystem;
 public class LiftCommand extends CommandBase {
     private LiftSubsystem subsystem;
     private int level;
-    public LiftCommand(LiftSubsystem subsystem){
+
+    public LiftCommand(LiftSubsystem subsystem) {
         this.subsystem = subsystem;
         level = 0;
         addRequirements(subsystem);
     }
+
     @Override
-    public void initialize(){
-        if(level == 0){
+    public void initialize() {
+        if (level == 0) {
             subsystem.setGoal(0.5);
             level++;
-        }else if(level == 1){
+        } else if (level == 1) {
             subsystem.setGoal(1);
             level++;
-        }else if(level == 2){
+        } else if (level == 2) {
             subsystem.setGoal(0);
             level = 0;
         }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/LiftSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/LiftSubsystem.java
@@ -5,19 +5,21 @@ import com.arcrobotics.ftclib.controller.wpilibcontroller.ProfiledPIDController;
 import com.arcrobotics.ftclib.hardware.motors.Motor;
 import com.arcrobotics.ftclib.trajectory.TrapezoidProfile;
 
-public class LiftSubsystem extends ProfiledPIDSubsystem{
+public class LiftSubsystem extends ProfiledPIDSubsystem {
     private Motor liftMotor;
-    private static double kS = 0, kG = 0,kV = 0, kA = 0;
+    private static double kS = 0, kG = 0, kV = 0, kA = 0;
     private final ElevatorFeedforward elevatorFeedForward = new ElevatorFeedforward(kS, kG, kV, kA);
     private static double kP = 1.0, kI = 0, kD = 0;
-    public LiftSubsystem(Motor liftMotor){
+
+    public LiftSubsystem(Motor liftMotor) {
         super(new ProfiledPIDController(kP, kI, kD, new TrapezoidProfile.Constraints(3.0, 3.0)), 0.0);
         this.liftMotor = liftMotor;
         liftMotor.encoder.setDistancePerPulse(2);
         setGoal(0.0);
     }
+
     @Override
-    public void useOutput(double output, TrapezoidProfile.State setPoint){
+    public void useOutput(double output, TrapezoidProfile.State setPoint) {
         double feedForward = elevatorFeedForward.calculate(setPoint.velocity);
         liftMotor.set(output + feedForward);
     }
